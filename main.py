a9ed3c6613dda966bf8ee1790040907b04ce5cef
ThanuMahee12 <mtthanu126@gmail.com>
2025-10-04 13:38:43 -0400

    feat:project initilization with nessary files

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from file-system-watcher!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ watcher/main.py
@@ -0,0 +1,3 @@
+if __name__=="__main__":
+    from watcher import main
+    main()
